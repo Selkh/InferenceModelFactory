193f1cdd78c2b649be542f5bb9ac667516613af2
xiaoying.zhang <xiaoying.zhang@enflame-tech.com>
2022-10-24 20:25:47 +0800

    [Feature](ARD-555) application: Integrate the classification models and refactor the directory structure.
    Root Cause:
            N/A
    Solution:
            Refactor
    Test:
            N/A
    Impact area:
            models/TopsModels
    Fix status:
            N/A

---
+++ onnx_models/classification/mobilenet/mobilenetv1.py
@@ -0,0 +1,51 @@
+"""
+/* Copyright 2018 The Enflame Tech Company. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+"""
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from onnx_models.classification.classification import ClassificationModel
+from onnx_models.base import OnnxModelFactory
+from common.data_process.img_preprocess import img_resize, img_center_crop
+import numpy as np
+
+
+class MobileNetV1Factory(OnnxModelFactory):
+    model = "mobilenetv1"
+
+    def new_model():
+        return MobileNetV1()
+
+
+class MobileNetV1(ClassificationModel):
+    def __init__(self):
+        super(MobileNetV1, self).__init__()
+
+    def preprocess(self, item):
+        width = int(self.options.get_input_width())
+        height = int(self.options.get_input_height())
+        input_size = (width, height)
+
+        image = img_resize(item.data, self.options.get_resize_size())
+        image = img_center_crop(image, input_size)
+        image_data = np.array(image, dtype='float32')
+
+        norm_image_data = (image_data / 255 - 0.5) * 2
+        norm_image_data = norm_image_data.reshape(
+            height, width, 3).astype('float32')
+        norm_image_data = np.array(norm_image_data).transpose(2, 0, 1)
+        item.data = norm_image_data
+        return item
---
+++ onnx_models/classification/torchvision/efficientnet/efficientnet.py
@@ -0,0 +1,49 @@
+"""
+/* Copyright 2018 The Enflame Tech Company. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+"""
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from onnx_models.classification.torchvision.torchvision import TorchVisionModel
+from onnx_models.base import OnnxModelFactory
+import numpy as np
+from scipy.special import softmax
+
+
+class EfficientNetTVFactory(OnnxModelFactory):
+    model = "efficientnet-tv"
+
+    def new_model():
+        return EfficientNetTV()
+
+
+class EfficientNetTV(TorchVisionModel):
+    def __init__(self):
+        super(EfficientNetTV, self).__init__()
+
+
+    def postprocess(self, item):
+        item.res = np.expand_dims(item.res, axis=0)
+        item.res = softmax(item.res, axis=1)
+        item.label = np.expand_dims(item.label, axis=0)
+
+        pred = np.argmax(item.res, axis=-1)
+        acc1 = 1 if pred == item.label else 0
+
+        indices = self.arg_topk(item.res)
+        acc5 = (item.label[..., None] == indices).any(axis=-1).sum()
+
+        return acc1, acc5
